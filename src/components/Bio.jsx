3f7d65768dc2f64a71a20c5d9dd0ab2651f8e421
magsca <carolmagalhaesjf@gmail.com>
2022-11-09 18:46:39 -0300

    editando a foto da bio

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -6,13 +6,6 @@ function Bio(){
         description="Oi, eu sou a Carol, Desenvolvedora de software em construção.
         Apaixonada por tecnologia e inovação, ligada no 220 volts, puro suco do alto astral!"
         imgPath="./src/components/img/carol13.jpeg" />
-
-       /* <>
-        <h2>Caroline Magalhães</h2>
-        <p> Oi, eu sou a Carol, Desenvolvedora front-end em construção.
-Apaixonada por tecnologia e inovação, ligada no 220 volts, puro suco do alto astral!</p>
-        <img src="https://media.tenor.com/fyimEFd5D0sAAAAM/nazare.gif" width="300" />
-        </> */
     )
 }
 export default Bio;
