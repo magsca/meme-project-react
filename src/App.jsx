160b3dba25ace80d9603b97db924285edcb3058a
magsca <carolmagalhaesjf@gmail.com>
2022-11-14 10:36:31 -0300

    realocando a foto na pasta assets

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import Header from './components/Header'
 import Bio from './components/Bio'
 import Cards from './components/Cards'
 import Footer from './components/Footer'
+
 import './App.css'
 
 
 function App() {
     return (
     <div className="App">
-  
     <Header title="Quem é você na vida hoje?"/> 
     <Bio />
     <Cards />
